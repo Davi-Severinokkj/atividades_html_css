09a7faf0074e7a59eea8dcade952ceab431344a9
Davi-Severinokkj <daviseverinoinf2@gmail.com>
2025-08-18 15:47:34 -0300

    práticas

---
+++ HTML E CSS/prática dia 14.08.2025/script/script.js
@@ -0,0 +1,25 @@
+// var - deixou de ser recomendado, mas ainda é usado
+// let - escopo de bloco, recomendado para variáveis que mudam. Consequentemente pode sofrer alteração!
+// const -  escopo de bloco, recomendado para constantes
+
+var nome = 'João'; // variável global
+
+let nome2 = 'Davi'; 
+
+const nome3 = 'Pedro';
+
+let anoNascimento = 2008;
+
+let x = 5;
+
+x = x + 10;
+
+print(x);
+
+const PI = 3.1415;
+let nome = "Avogrado";
+let idade = 16;
+let altura = 1.75;
+let cidadeNatal = "Paris";
+
+console.log("Meu nome é" + nome + ", eu matarei todos vocês, tenho " + idade + " anos. Nasci em " + cidadeNatal)
